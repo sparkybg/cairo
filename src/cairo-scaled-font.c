79ad8e69273424b2d0b192720e127ebede8fff15
Spartak Timchev <sparky@omegatim.com>
2021-06-08 21:16:33 +0300

    aaa

--- src/cairo-scaled-font.c
+++ src/cairo-scaled-font.c
@@ -1141,6 +1141,7 @@ cairo_scaled_font_create (cairo_font_face_t          *font_face,
 	}
     }
 
+	printf("font_face->backend->scaled_font_create\n"); //sparky
     status = font_face->backend->scaled_font_create (font_face, font_matrix,
 						     ctm, options, &scaled_font);
     /* Did we leave the backend in an error state? */
